1e57708a7db533b406d05b103a54e289b4dedff8
Robith Yusuf AlFahsa <robithyusufalfahsa@gmail.com>
2024-08-03 21:31:52 +0700

    updated

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ app.post("/roast", async (req, res) => {
 				". (provide the response in English and do not provide praise or advice and do not use explicit words)";
 		}
 
-		const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+		const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 		const result = await model.generateContent(prompt);
 		const response = await result.response;
 
